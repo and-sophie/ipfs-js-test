93b5faa32a4f049057a60316ce080aff025b2c9b
Sophie Gill <sgill@andigital.com>
2017-04-27 10:24:51 +0100

    :dancer: adds json files to ipfs

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ let ipfs = require('ipfs-js');
 
 ipfs.setProvider(require('ipfs-api')('localhost', '5001'));
 
-console.log('Adding string: MedToken');
+console.log('Setting up test string: MedToken');
 
 ipfs.add("MedToken", function(err, hash) {
+    console.log("Adding string to IPFS...")
     if (err) throw err; // If connection is closed
-    console.log('Test added with hash: ' + hash);
+    console.log('Test string added with hash: ' + hash);
     ipfs.cat(hash, function(err, buffer) {
+        console.log("Retrieving string from IPFS...")
         if (err) throw err;
-        console.log('String gotten from IPFS: ' + buffer.toString());
+        console.log('String returned from IPFS: ' + buffer.toString());
     });
 });
-
 
 let testJson = {
   "Name": "Test Person",
@@ -21,4 +22,26 @@ let testJson = {
     "Email Address": "blahblah@test.com"
 };
 
-console.log(JSON.stringify(testJson));
+console.log("Setting up test JSON: " + JSON.stringify(testJson));
+
+ipfs.addJson(testJson, function(err, hash) {
+  console.log("Adding JSON to ipfs...")
+  if (err) throw err;
+  console.log('JSON addded with hash: ' + hash);
+  ipfs.catJson(hash, function(err, buffer) {
+    console.log("Retrieving json from IPFS...");
+    if (err) throw err;
+    console.log('JSON returned from IPFS: ' + JSON.stringify(buffer))
+  })
+})
+
+
+console.log("Setting up test image from URL...");
+
+let testImg = "http://weknowmemes.com/wp-content/uploads/2013/04/an-adventure-alpaca-my-bags.jpg";
+
+ipfs.api.util.addFromURL(testImg, function(err, hash) {
+  console.log("Adding image from URL...")
+  if (err) throw err;
+  console.log("Image added with hash: " + hash);
+})
